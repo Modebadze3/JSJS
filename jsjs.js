53bfe7ea2afe8b4ee1ad3a5784a5dfb175d1bc9f
Modebadze3 <72819119+Modebadze3@users.noreply.github.com>
2020-11-07 00:33:08 +0400

    update

--- jsjs.js
+++ jsjs.js
@@ -1,7 +1,9 @@
 // Task1
 
 let array = [5, 10, 25, 100, 6, 18];
-let result = array.map(function(item) {
-    return item /2;
-})
+let result = array.map(item => item / 2);
 console.log(result)
+
+// Task2
+
+let numbers = [5, 25, 89, 120, 36]
