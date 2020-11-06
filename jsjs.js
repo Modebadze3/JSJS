117db7484d7e6cd7afab02d3ce1ad44493530dc3
Modebadze3 <72819119+Modebadze3@users.noreply.github.com>
2020-11-07 02:38:26 +0400

    add 4

--- jsjs.js
+++ jsjs.js
@@ -1,9 +1,31 @@
-// Task1
+// // // Task1
 
-let array = [5, 10, 25, 100, 6, 18];
-let result = array.map(item => item / 2);
+// let array = [5, 10, 25, 100, 6, 18];
+// let result = array.map(item => item / 2);
+// console.log(result)
+
+// // Task2
+
+// let numbers = [5, 25, 89, 120, 36]
+// numbers.push('javascript','python')
+// numbers.unshift('html', 'css')
+// console.log(numbers);
+// numbers.pop()
+// numbers.shift()
+// console.log(numbers);
+
+// Task3
+
+// let array2 = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
+// let result = array2.filter(item => typeof item == 'string')
+// console.log(result);
+
+// Task4
+
+let array3 = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
+let result = array3.filter(item => typeof item != 'string')
 console.log(result)
 
-// Task2
 
-let numbers = [5, 25, 89, 120, 36]
+
+
