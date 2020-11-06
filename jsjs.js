69e19bfc452e7de776d4b3e98ca3e7bdba9f01fe
Modebadze3 <72819119+Modebadze3@users.noreply.github.com>
2020-11-07 03:03:35 +0400

    done

--- jsjs.js
+++ jsjs.js
@@ -1,4 +1,4 @@
-// // // Task1
+// Task1
 
 // let array = [5, 10, 25, 100, 6, 18];
 // let result = array.map(item => item / 2);
@@ -14,17 +14,42 @@
 // numbers.shift()
 // console.log(numbers);
 
-// Task3
+// // Task3
 
 // let array2 = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
 // let result = array2.filter(item => typeof item == 'string')
 // console.log(result);
 
-// Task4
+// // Task4
 
-let array3 = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
-let result = array3.filter(item => typeof item != 'string')
-console.log(result)
+// let array3 = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
+// let result = array3.filter(item => typeof item != 'string')
+// console.log(result)
+
+// // Task5
+
+// let cities = [
+//     {name: 'los angeles', population: 3792621},
+//     {name: 'new york', population: 8175133},
+//     {name: 'chicago', population: 2695598},
+//     {name: 'houston', population: 2099451},
+//     {name: 'philadelphia', population: 1526006}
+// ];
+// let result = cities.filter(i => i.population < 3000000)
+// console.log(result)
+
+// // Task6
+
+// let frt = ['ფორთოხალი', 'ბანანი', 'მსხალი']
+// console.log(frt)
+// frt.push('ვაშლი', 'ანანასი')
+// frt.unshift('საზამთრო')
+// console.log(frt)
+// frt.shift()
+// frt.pop()
+// console.log(frt)
+
+
 
 
 
